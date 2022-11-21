d63dcf5a4fe96a4f4e8db3d16d0e05c90d16ca3a
김민준 <80810346+minjun223@users.noreply.github.com>
2022-11-21 17:59:35 +0900

    완주하지 못한 선수 문제
    자바

---
+++ Test/solution.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+class solution {
+    public String solution(String[] participant, String[] completion) {//프로그램어스 변수에 값을 넣어줌
+
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        int i;
+        for ( i=0; i<completion.length; i++){//completion의 길이 만큼 반복
+
+            if (!participant[i].equals(completion[i])){
+                return participant[i];
+            }
+        }
+        return participant[i];
+    }
+}
